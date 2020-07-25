62581c7c9ea625c088fddcce505709b44eab6059
Mosharraf Musa <mosharrafny@gmail.com>
2020-07-24 21:15:40 -0400

    first commit

---
+++ routers/users-model.js
@@ -0,0 +1,34 @@
+const db = require("../data/db-config");
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+  remove,
+};
+
+function find() {
+  return db("users").select("id", "username", "password", "email");
+}
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+function add(user) {
+  return db("users")
+    .insert(user, "id")
+    .then((ids) => {
+      const [id] = ids;
+      return findById(id);
+    });
+}
+
+function findById(id) {
+  return db("users").where({ id }).first();
+}
+
+function remove(id) {
+  return db("users").where({ id }).del();
+}
